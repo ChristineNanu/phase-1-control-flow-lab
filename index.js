8aa047c3d76818f8daea4d8d00c7047c90ab02e7
mundi <christine.mundi@student.moringaschool.com$ git config --global user.email christine.mundi@student.moringaschool.com>
2023-03-15 11:56:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+const {expect}= require ("chai");
+function scuberGreetingForFeet(ride){
+
   // Write your code here!
+  if( ride<400){
+  return 'This one is on me!';
+  }
+  else if(ride>2000 && ride <2500) {
+  return  "I will gladly take your thirty bucks.";
+  }
+  else if (ride>2500){
+    return "No can do."
+  }
+
+  }
+
+  
+
+
+function ternaryCheckCity (city){
+
+  
+return city === "NYC" ? "Ok, sounds good."
+:"No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(Tip){
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(Tip) {
+    case 'generous':
+      return'Thank you so much.';
+      case 'not as generous':
+        return 'Thank you.';
+        default:
+          return 'Bye.';
+  }
 }
